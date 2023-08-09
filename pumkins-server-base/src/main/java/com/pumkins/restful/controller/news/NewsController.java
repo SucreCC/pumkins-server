ec2d67b9a3f05a72fd10252573435fad91073048
dengkai <dengkai@wxchina.com>
2023-08-09 11:42:52 +0800

    hot news

--- pumkins-server-base/src/main/java/com/pumkins/restful/controller/news/NewsController.java
+++ pumkins-server-base/src/main/java/com/pumkins/restful/controller/news/NewsController.java
@@ -1,10 +1,14 @@
 package com.pumkins.restful.controller.news;
 
+import com.pumkins.dto.resp.NewsResp;
+import com.pumkins.dto.response.JsonResp;
 import com.pumkins.restful.service.news.NewsService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author: dengKai
@@ -12,8 +16,14 @@ import javax.annotation.Resource;
  * @description: TODO
  */
 @RestController
-@RequestMapping("/pumkins")
+@RequestMapping("/pumkins/news")
 public class NewsController {
     @Resource
     private NewsService newsService;
+
+    @GetMapping("/get-hot-topic")
+    private JsonResp<List<NewsResp>> getHotTopic(){
+        List<NewsResp> news = newsService.getNews();
+        return JsonResp.success(news);
+    }
 }
